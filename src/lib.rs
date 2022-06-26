612b89124a97564372b1052fcda987ba79cbc736
agabani <agabani@users.noreply.github.com>
2022-06-26 19:29:10 +0100

    feat: server executable

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
 #![warn(clippy::pedantic)]
+
+/// Error returned by most functions.
+///
+/// For performance reasons, boxing is avoided in any hot path.
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+/// A specialized `Result` type for this crate's operations.
+///
+/// This is defined as a convenience to make error handling easier.
+pub type Result<T> = std::result::Result<T, Error>;
